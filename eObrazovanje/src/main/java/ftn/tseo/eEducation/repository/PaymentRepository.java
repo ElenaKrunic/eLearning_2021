2ceccbbea9c6e2793ba9b7310da7070d355611c3
Milica Milivojevic <mmilivojevic68@gmail.com>
2021-03-20 12:09:29 +0100

    Dodate metode za sve isplate za jednog studenta

--- eObrazovanje/src/main/java/ftn/tseo/eEducation/repository/PaymentRepository.java
+++ eObrazovanje/src/main/java/ftn/tseo/eEducation/repository/PaymentRepository.java
@@ -1,9 +1,15 @@
 package ftn.tseo.eEducation.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import ftn.tseo.eEducation.model.FinancialCard;
 import ftn.tseo.eEducation.model.Payment;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
-
+	//to do
+	@Query
+	List<Payment> getFinancialCardPayment(Long id);
 }
--- eObrazovanje/src/main/java/ftn/tseo/eEducation/service/PaymentService.java
+++ eObrazovanje/src/main/java/ftn/tseo/eEducation/service/PaymentService.java
@@ -1,5 +1,26 @@
 package ftn.tseo.eEducation.service;
 
-public class PaymentService {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ftn.tseo.eEducation.model.FinancialCard;
+import ftn.tseo.eEducation.model.Payment;
+import ftn.tseo.eEducation.repository.PaymentRepository;
+
+@Service
+public class PaymentService {
+	
+	@Autowired
+	PaymentRepository paymentRepository;
+	
+	private List<Payment> getFinancialCardPayment(Long id){
+		List<Payment> payments = new ArrayList<Payment>();
+		for (Payment p: paymentRepository.getFinancialCardPayment(id)) {
+			payments.add(p);
+		}
+		return payments;
+	}
 }
