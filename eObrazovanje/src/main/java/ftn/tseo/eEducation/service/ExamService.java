793abf2f4d99cde535038521932d0a7ecfbe2564
mirjana <mirja@DESKTOP-2KGUPSS>
2021-03-20 12:11:08 +0100

    ExamService odradjen bez update

--- eObrazovanje/src/main/java/ftn/tseo/eEducation/service/ExamService.java
+++ eObrazovanje/src/main/java/ftn/tseo/eEducation/service/ExamService.java
@@ -1,9 +1,29 @@
 package ftn.tseo.eEducation.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import ftn.tseo.eEducation.model.Exam;
+import ftn.tseo.eEducation.repository.ExamRepository;
 
 @Service
 public class ExamService {
-	
-	
+
+	@Autowired
+	ExamRepository examRepository;
+
+	public List<Exam> findAll() {
+		return examRepository.findAll();
+	}
+
+	public Exam save(Exam exam) {
+		return examRepository.save(exam);
+	}
+
+	public void remove(Integer id) {
+		examRepository.deleteById(id);
+	}
+
 }
