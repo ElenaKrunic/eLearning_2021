fa2b9834a09872fe1122891b990218b746297812
mirjana <mirja@DESKTOP-2KGUPSS>
2021-03-20 11:19:05 +0100

    Dodat prazan exam service i exam repository

---
+++ eObrazovanje/src/main/java/ftn/tseo/eEducation/repository/ExamRepository.java
@@ -0,0 +1,11 @@
+package ftn.tseo.eEducation.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ftn.tseo.eEducation.model.Exam;
+
+
+
+public interface ExamRepository extends JpaRepository<Exam, Integer>{
+
+}
---
+++ eObrazovanje/src/main/java/ftn/tseo/eEducation/service/ExamService.java
@@ -0,0 +1,9 @@
+package ftn.tseo.eEducation.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExamService {
+	
+	
+}
