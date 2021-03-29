c1dec9cc6ccec2102ab6ad66b5367f416abd1e2b
Milica Milivojevic <mmilivojevic68@gmail.com>
2021-03-29 21:35:38 +0200

    Odradjen payment i payout dto

---
+++ eObrazovanje/src/main/java/ftn/tseo/eEducation/DTO/PayoutDTO.java
@@ -0,0 +1,63 @@
+package ftn.tseo.eEducation.DTO;
+
+import java.util.Date;
+
+import ftn.tseo.eEducation.model.PayOut;
+
+public class PayoutDTO {
+	
+	private String descriptionOfPayout;
+	private float amountOfPayout;
+	private Date dateOfPayout;
+	private FinancialCardDTO financialCardDTO;
+	
+	public PayoutDTO(PayOut payout) {
+		super();
+		this.descriptionOfPayout = payout.getDescriptionPayOut();
+		this.amountOfPayout = payout.getPayOutAmount();
+		this.dateOfPayout = payout.getDateOfPayOut();
+		this.financialCardDTO = new FinancialCardDTO(payout.getFinancialCard());
+	}
+
+	public PayoutDTO() {
+		super();
+
+
+	}
+
+	public String getDescriptionOfPayout() {
+		return descriptionOfPayout;
+	}
+
+	public void setDescriptionOfPayout(String descriptionOfPayout) {
+		this.descriptionOfPayout = descriptionOfPayout;
+	}
+
+	public float getAmountOfPayout() {
+		return amountOfPayout;
+	}
+
+	public void setAmountOfPayout(float amountOfPayout) {
+		this.amountOfPayout = amountOfPayout;
+	}
+
+	public Date getDateOfPayout() {
+		return dateOfPayout;
+	}
+
+	public void setDateOfPayout(Date dateOfPayout) {
+		this.dateOfPayout = dateOfPayout;
+	}
+
+	public FinancialCardDTO getFinancialCardDTO() {
+		return financialCardDTO;
+	}
+
+	public void setFinancialCardDTO(FinancialCardDTO financialCardDTO) {
+		this.financialCardDTO = financialCardDTO;
+	}
+	
+	
+	
+
+}
