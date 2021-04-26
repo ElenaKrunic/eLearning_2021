c064a0696e29c4334aaa2540022f4fbdea2c3667
Milica Milivojevic <mmilivojevic68@gmail.com>
2021-04-26 19:38:24 +0200

    odradjen model za finansijsku karticu

--- eOFRONT/src/app/model/financial-card.ts
+++ eOFRONT/src/app/model/financial-card.ts
@@ -1,2 +1,37 @@
+
+import { Student } from "./student";
+
 export class FinancialCard {
+
+    public id?:number;
+	
+	public initialState:number;
+	
+	public totalPayment:number;
+	
+	public  totalPayout:number;
+	public totalCost:number;
+    public student:Student;
+
+    constructor(financialCardConf:FinancialCardInterface){
+        this.id=financialCardConf.id;
+        this.initialState=financialCardConf.initialState;
+        this.totalPayment=financialCardConf.totalPayment;
+        this.totalPayout=financialCardConf.totalPayout;
+        this.totalCost=financialCardConf.totalCost;
+        this.student=financialCardConf.student;
+    }
+
+	
+
+
+
+}
+interface FinancialCardInterface{
+    id?:number;
+    initialState:number;
+    totalPayment:number;
+    totalPayout:number;
+    totalCost:number;
+    student:Student;
 }
