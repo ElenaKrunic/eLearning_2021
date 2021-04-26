88d2a93e99f9b10e9ac9661c2b76dfd7a659bea8
Milica Milivojevic <mmilivojevic68@gmail.com>
2021-04-26 19:52:29 +0200

    Odradjeni ostali delovi modela

--- eOFRONT/src/app/model/document-type.ts
+++ eOFRONT/src/app/model/document-type.ts
@@ -1,2 +1,16 @@
-export class DocumentType {
+export class DocumentType implements DocumentTypeInterface {
+    public id?:number;
+    public name:String;
+    public code:String;
+
+    constructor(documentTypeConf:DocumentTypeInterface){
+        this.id=documentTypeConf.id;
+        this.name=documentTypeConf.name;
+        this.code=documentTypeConf.code;
+    }
+}
+interface DocumentTypeInterface{
+    id?:number;
+    name:String;
+    code:String;
 }
