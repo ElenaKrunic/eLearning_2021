888dfe4220127cfc2f6842cebf76d2ba67eb98aa
Milica Milivojevic <mmilivojevic68@gmail.com>
2021-05-10 13:10:58 +0200

    Izmene u fajlovima za login i routing

--- eOFRONT/src/app/routing/routes.ts
+++ eOFRONT/src/app/routing/routes.ts
@@ -1,13 +1,15 @@
 import { Routes } from "@angular/router";
 import { LoginComponent } from "../login/login.component";
+import { LoginGuard } from "../login/login.service";
 import { RoleGuard } from "../login/role.service";
 
 export const routes: Routes = [
+
+	
 	{
 		path: 'login',
 		component: LoginComponent,
-		canActivate: [RoleGuard],
-		data: {expectedRoles: 'ADMIN|STUDENT|PROFFESOR'}
+		canActivate: [LoginGuard],
 	},
 	
 ];
