8564ace94c2cec884553bc3506bcdfe2409e74cc
dunjajazavac <martinovicdunja20@gmail.com>
2021-05-10 20:50:59 +0200

    dodat u profesorServicu getProfessor metoda

--- eOFRONT/src/app/professor/professor.service.ts
+++ eOFRONT/src/app/professor/professor.service.ts
@@ -3,6 +3,7 @@ import { Observable, Subject } from 'rxjs';
 import { HttpResponse, HttpClient } from '@angular/common/http';
 import { Course } from '../model/course';
 import { Student } from '../model/student';
+import { Professor } from '../model/professor';
 
 @Injectable()
 export class ProfessorService {
@@ -18,6 +19,11 @@ export class ProfessorService {
     announceChange() {
         this.RegenerateData.next();
     }
+
+    getProfessor(id:number):Observable<HttpResponse<Professor[]>>{
+      const url=`${this.path}/{id}`;
+      return this.http.get<Professor[]>(url, {observe: 'response'});
+    } 
 
 
     getProfessorStudents(professorId:number): Observable<HttpResponse<Student[]>> {
