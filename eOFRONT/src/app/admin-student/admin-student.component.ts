751efd85e7d0449a6fd24118cb9c53648e6bd250
Elena Krunic <elenakrunic@gmail.com>
2021-05-31 12:35:43 +0200

    Napisan kod za CRUD operacije nad studentom u admin-studentu i kreirana odgovarajuca html stranica

--- eOFRONT/src/app/admin-student/admin-student.component.ts
+++ eOFRONT/src/app/admin-student/admin-student.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Student } from '../model/student';
+import { StudentService } from '../students/student.service';
 
 @Component({
   selector: 'app-admin-student',
@@ -7,9 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminStudentComponent implements OnInit {
 
-  constructor() { }
+  students: Student[];
+  subscription: Subscription;
+
+  constructor(private studentService: StudentService, private router: Router) { 
+    this.getStudents();
+  }
 
   ngOnInit(): void {
+    this.getStudents();
+  }
+
+  getStudents(){
+    this.studentService.getStudents().subscribe(res => 
+      this.students = res.body);
+  }
+
+  gotoAdd() : void {
+    this.router.navigate(["/addStudent"]);
+  }
+
+  gotoEdit(student : Student) : void {
+    this.router.navigate(['/editStudent', student.id]);
+  } 
+
+  deleteStudent(studentId : number) : void {
+    this.studentService.deleteStudent(studentId).subscribe(
+      () => this.getStudents()
+    );
   }
 
 }
