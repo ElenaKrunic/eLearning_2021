bc4bac950c9409c0ea0b982eb99217a03889c6b6
Milica Milivojevic <mmilivojevic68@gmail.com>
2021-06-23 11:20:53 +0200

    izbrisane biblioteke i zakomentarisam main component radi iz appa sve

--- eOFRONT/src/app/main/main.component.ts
+++ eOFRONT/src/app/main/main.component.ts
@@ -10,40 +10,40 @@ import { JwtHelperService } from '@auth0/angular-jwt';
 })
 export class MainComponent {
 
-<<<<<<< HEAD
-  	// public roles: string[]=[];
-	// currentUser:any;
-	// constructor(private router: Router) {}
-=======
+// <<<<<<< HEAD
+//   	// public roles: string[]=[];
+// 	// currentUser:any;
+// 	// constructor(private router: Router) {}
+// =======
 
 
-  public role?: string;
->>>>>>> f721dbc5c2f5a7c192403527e84cf48f156e698c
+//   public role?: string;
+// >>>>>>> f721dbc5c2f5a7c192403527e84cf48f156e698c
 
-	// // checkRole() {
-	// // 	const item = localStorage.getItem('currentUser');
+// 	// // checkRole() {
+// 	// // 	const item = localStorage.getItem('currentUser');
 
-	// // 	if (!item) {
-	// // 		this.router.navigate(['login']);
-	// // 		this.role = undefined;
-	// // 		return;
-	// // 	}
+// 	// // 	if (!item) {
+// 	// // 		this.router.navigate(['login']);
+// 	// // 		this.role = undefined;
+// 	// // 		return;
+// 	// // 	}
 
-	// // 	const jwt: JwtHelperService = new JwtHelperService();
-	// // 	this.role = jwt.decodeToken(item).roles[0];
-	// // }
-	// ngOnInit(){
-	// 	this.roles=this.getCurrentUser.roles();
-	// 	this.currentUser=this.getCurrentUser();
+// 	// // 	const jwt: JwtHelperService = new JwtHelperService();
+// 	// // 	this.role = jwt.decodeToken(item).roles[0];
+// 	// // }
+// 	// ngOnInit(){
+// 	// 	this.roles=this.getCurrentUser.roles();
+// 	// 	this.currentUser=this.getCurrentUser();
 
-	// }
-	// getCurrentUser(){
-	// 	if(localStorage.getItem('currentUser')){
-	// 		return JSON.parse(localStorage.currentUser);
-	// 	}
-	// 	else{
-	// 		return false;
-	// 	}
+// 	// }
+// 	// getCurrentUser(){
+// 	// 	if(localStorage.getItem('currentUser')){
+// 	// 		return JSON.parse(localStorage.currentUser);
+// 	// 	}
+// 	// 	else{
+// 	// 		return false;
+// 	// 	}
 
-	// }
+// 	// }
 }
