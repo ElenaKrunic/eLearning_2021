caf3ca36ddcbf414470144589910cf8550177e3c
Elena Krunic <elenakrunic@gmail.com>
2021-07-01 16:31:04 +0200

    Uradjena validacija prilikom dodavanja placanja za admina

--- eOFRONT/src/app/admin-add-payment/admin-add-payment.component.ts
+++ eOFRONT/src/app/admin-add-payment/admin-add-payment.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, AbstractControl, Validators } from '@angular/forms';
 import { Payment } from '../model/payment';
 import { PaymentService } from '../payment/payment.service';
+
 
 @Component({
   selector: 'app-admin-add-payment',
@@ -16,11 +18,53 @@ export class AdminAddPaymentComponent implements OnInit {
     financialCard : null
   });
 
+  form! : FormGroup; 
   submitted = false;
 
-  constructor(private paymentService: PaymentService) { }
+  constructor(private paymentService: PaymentService, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      dateOfPayment: [
+        '',
+        [
+          Validators.required
+        ] 
+      ],
+      paymentDescription: [
+        '',
+       [
+        Validators.required, 
+       ]
+      ],
+      paymentAmount: [
+        '',
+        [
+          Validators.required, 
+          Validators.minLength(3),
+          Validators.maxLength(9)
+        ] 
+      ],
+    });
+  }
+
+  get f(): { [key: string]: AbstractControl} {
+    return this.form.controls;
+  }
+
+  onSubmit() : void {
+    this.submitted = true; 
+
+    if(this.form.invalid) {
+      return; 
+    }
+
+    console.log(JSON.stringify(this.form.value, null, 2));
+  }
+
+  onReset() : void {
+    this.submitted = false; 
+    this.form.reset();
   }
 
   savePayment() {
