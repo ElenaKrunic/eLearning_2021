c274c4cd67168b4c4c1b258b2fb45d8c2e08ed68
dunjajazavac <martinovicdunja20@gmail.com>
2021-07-08 18:13:11 +0200

    .

--- eObrazovanje/src/main/java/ftn/tseo/eEducation/repository/PaymentRepository.java
+++
@@ -1,8 +0,0 @@
-package ftn.tseo.eEducation.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ftn.tseo.eEducation.model.Payment;
-
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-
-}
--- eObrazovanje/src/main/java/ftn/tseo/eEducation/repository/PreexamObligationStatusRepository.java
+++
@@ -1,9 +0,0 @@
-package ftn.tseo.eEducation.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import ftn.tseo.eEducation.model.PreexamObligationStatus;
-
-public interface PreexamObligationStatusRepository extends JpaRepository<PreexamObligationStatus, Long>{
-
-}
