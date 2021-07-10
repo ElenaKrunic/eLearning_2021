f7689646a89f7ffae8bcc3a0bbadded596e74dc6
Milica Milivojevic <mmilivojevic68@gmail.com>
2021-07-11 01:50:20 +0200

    Dodavanje u security

--- eObrazovanje/src/main/java/ftn/tseo/eEducation/security/SecurityConfiguration.java
+++ eObrazovanje/src/main/java/ftn/tseo/eEducation/security/SecurityConfiguration.java
@@ -104,7 +104,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 				.and()
 			.authorizeRequests()
-				.antMatchers( "/","/api/login","/styles.css","/runtime.js","/polyfills.js","/vendor.js","/main.js","/favicon.ico","/styles.css.map","/runtime.js.map","/polyfills.js.map","/vendor.js.map","/main.js.map").permitAll() 
+				.antMatchers( "/","/api/login","/styles.css","/runtime.js","/polyfills.js","/vendor.js","/main.js","/favicon.ico","/styles.css.map","/runtime.js.map","/polyfills.js.map","/vendor.js.map","/main.js.map","/bootstrap.css","/bootstrap.css.map").permitAll() 
 				.antMatchers(HttpMethod.POST, "/api/**")
 				.hasAuthority("ROLE_ADMIN")
 				.anyRequest().authenticated();
