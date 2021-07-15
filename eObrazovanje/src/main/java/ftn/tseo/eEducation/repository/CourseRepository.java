dddee106d9dded7580cd022798e3a5e72114278d
mirjana <mirja@DESKTOP-2KGUPSS>
2021-07-15 22:35:32 +0200

    dodati pagevi za course,exam i professor repo

--- eObrazovanje/src/main/java/ftn/tseo/eEducation/repository/CourseRepository.java
+++ eObrazovanje/src/main/java/ftn/tseo/eEducation/repository/CourseRepository.java
@@ -3,13 +3,19 @@ package ftn.tseo.eEducation.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import ftn.tseo.eEducation.model.Course;
+import ftn.tseo.eEducation.model.ExamPeriod;
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
 	
 	
-	
+	Page<Course> findAll(Pageable pageable);
+	Page<Course> findByTitle(String title, Pageable page);
+	List<Course> findByTitle(String title, Sort sort);
 }
