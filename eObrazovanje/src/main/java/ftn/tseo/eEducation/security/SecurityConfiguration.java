6474338f5fb1b5ab01ee04a5011d8bf2fafa5573
mirjana <mirja@DESKTOP-2KGUPSS>
2021-07-16 05:12:30 +0200

    f

--- eObrazovanje/src/main/java/ftn/tseo/eEducation/security/SecurityConfiguration.java
+++ eObrazovanje/src/main/java/ftn/tseo/eEducation/security/SecurityConfiguration.java
@@ -105,7 +105,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 				.and()
 			.authorizeRequests()
 
-				.antMatchers( "/","/api/examPeriods/**","/api/financialCards/**", "/api/payments/**",  "/api/preexamObligations/**" , "/api/students/**", "/api/courses/**","api/exams/**","api/professors" ,
+				.antMatchers( "/","/api/examPeriods/**","/api/financialCards/**", "/api/payments/**",  "/api/preexamObligations/**" , "/api/students/**", "/api/courses/**","/api/exams/**","/api/professors" ,
 						"/api/login","/styles.css","/runtime.js","/polyfills.js","/vendor.js","/main.js","/favicon.ico","/styles.css.map","/runtime.js.map","/polyfills.js.map","/vendor.js.map","/main.js.map").permitAll() 
 //
 //				.antMatchers( "/","/api/login","/styles.css","/runtime.js","/polyfills.js","/vendor.js","/main.js","/favicon.ico","/styles.css.map","/runtime.js.map","/polyfills.js.map","/vendor.js.map","/main.js.map","/bootstrap.css","/bootstrap.css.map").permitAll() 
